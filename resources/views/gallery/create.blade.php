87c287daddc50b185e52c72edc394b13ad855cbb
unknown <andreabcm16@gmail.com>
2021-12-20 23:42:35 +0100

    creating create form

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -1,1 +1,16 @@
 Form para añadir imágenes
+
+<form action="" method="post" enctype="multipart/form-data">
+
+    <label for="Photo"> Photo </label>
+    <input type="file" name="Photo" id="Photo">
+    <br>
+
+    <label for="Title"> Title </label>
+    <input type="text" name="Title" id="Title">
+    <br>
+
+    <label for="Enviar"> Enviar </label>
+    <input type="submit" name="Enviar" id="Enviar">
+
+</form>
