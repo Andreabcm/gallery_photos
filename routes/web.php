7745fa03e625ccce5c94d469053c37a6c5665d60
unknown <andreabcm16@gmail.com>
2021-12-20 23:30:35 +0100

    routing all views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GalleryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('gallery',GalleryController::class);
 
 
 
