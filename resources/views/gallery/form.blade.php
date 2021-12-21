a59ad173e0e8c55a706f80e068c1cffa0218dfda
unknown <andreabcm16@gmail.com>
2021-12-21 13:16:53 +0100

    adjusting forms

--- resources/views/gallery/form.blade.php
+++ resources/views/gallery/form.blade.php
@@ -1,10 +1,12 @@
 
     <label for="Image"> Image </label>
+    @if(isset($gallery->Image))
     <img src="{{ asset('storage').'/'.$gallery->Image }}" alt="" width="100">
+    @endif
     <input type="file" name="Image" value=" " id="Image">
     <br>
     <label for="Title"> Title </label>
-    <input type="text" name="Title" value="{{ $gallery->title }}" id="Title">
+    <input type="text" name="Title" value="{{ isset($gallery->title) ?$gallery->title:'' }}" id="Title">
     <br>
     <input type="submit" value="Save">
     <br>
