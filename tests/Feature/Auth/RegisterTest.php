71baa89e289f11f2726cf7dc07c514b705ab605d
unknown <andreabcm16@gmail.com>
2021-12-21 15:22:14 +0100

    doing test resgiter path confirmation

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_register_path_confirmation()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
+}
