a91874575b65b0812dc3da1159c8d644c33b36a5
unknown <andreabcm16@gmail.com>
2021-12-21 16:13:32 +0100

    configuring authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GalleryController;
 
@@ -15,15 +16,15 @@ use App\Http\Controllers\GalleryController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 
 Route::resource('gallery',GalleryController::class);
-
-
-
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [GalleryController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', [GalleryController::class, 'index'])->name('home');
+});
