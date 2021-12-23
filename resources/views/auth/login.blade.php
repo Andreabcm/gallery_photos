d2f4bbc1bf367aca9f20808a532d5b2d0ee24ab2
unknown <andreabcm16@gmail.com>
2021-12-23 10:04:38 +0100

    making login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +70,110 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <link href="{{asset('css/login.css')}}" rel="stylesheet"/>
+        <title>Login</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            {
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+            }
+            body{
+            font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+
+    <body class="container-body-login d-flex align-items-center">
+        <div class="container contenedor-login">
+            <div class="row justify-content-center ">
+                <div class="col-md-8 justify-content-center">
+                    <div class="card-body-login card  d-flex justify-content-center">
+
+                        <div class="card-body formulario-card">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+
+                                <div class="form-group row">
+                                    <label for="email" class="color-label col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="form-group row">
+                                    <label for="password" class="color-label col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                            <label class="color-label form-check-label" for="remember">
+                                                {{ __('Remember Me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="d-flex justify-content-center form-group row mb-0">
+                                    <div class=" col-md-8 offset-md-4 w-100">
+                                        <button type="submit" class="btn btn-outline-light mr-4">
+                                            {{ __('Login') }}
+                                        </button>
+
+                                        @if (Route::has('password.request'))
+                                            <a class="password-forgot color-label btn btn-link" href="{{ route('password.request') }}">
+                                                {{ __('Forgot Your Password?') }}
+                                            </a>
+                                        @endif
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row justify-content-center">
+                    <a class="enlace-register" href="{{ route('register') }}"> <button class="btn btn-outline-light boton-create-account">crear cuenta nueva</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <script src="{{asset('js/app.js')}}">
+    </script>
+    </body>
+    </html>
